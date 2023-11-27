0ec74832f615406edde822a870698a30cf759e47
TsarProgrammerII <mirandacm@g.cofc.edu>
2023-11-26 23:04:22 -0500

    new file:   frontend/public/Colby.jpg 	new file:   frontend/public/Kaylee.jpg 	new file:   frontend/public/Leo.jpg 	new file:   frontend/public/LiterallyMe.jpg 	new file:   frontend/src/pages/About.css 	modified:   frontend/src/pages/About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
+import './About.css';
 
 const About = () => {
     return (
         <div>
             <Sidebar />
-            <h1>About</h1>
+            <h1>About Us</h1>
+
+            <div className="image-container">
+                <div className="image">
+                    <img src="/LiterallyMe.jpg" alt="Photo 1" />
+                    <h3 className="image-header">
+                        Schizophrenic
+                        <p className="image-text">Cogito Ergo Sum</p>
+                    </h3>
+                </div>
+                <div className="image">
+                    <img src="/Colby.jpg" alt="Photo 2" />
+                    <h3 className="image-header">
+                        Average Comp Sci Enjoyer
+                        <p className="image-text">Also Doctor Who fanatic</p>
+                    </h3>
+                </div>
+                <div className="image">
+                    <img src="/Kaylee.jpg" alt="Photo 3" />
+                    <h3 className="image-header">
+                        Masters in CS
+                        <p className="image-text">Does not know how to replace <br/> smoke alarm batteries</p>
+                    </h3>
+                </div>
+                <div className="image">
+                    <img src="/Leo.jpg" alt="Photo 4" />
+                    <h3 className="image-header">
+                        ???
+                        <p className="image-text">Brazilians can double jump</p>
+                    </h3>
+                </div>
+            </div>
         </div>
     )
 }
